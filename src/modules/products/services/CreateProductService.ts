eb4e23c478dd2c9fec0b16e15f20466df57c186a
Vinicius <vinigugelmin@hotmail.com>
2021-12-16 11:02:42 -0300

    CreateProductService

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
+import AppError from '@shared/errors/AppError';
+import Product from '@modules/products/typeorm/entities/Product';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError(`Product with name ${name} already exists`);
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
