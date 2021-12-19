b5acc27e08fbe5d7807d04c5a77fecaacae5747b
Vinicius <vinigugelmin@hotmail.com>
2021-12-19 15:50:32 -0300

    Setting user.id to request that use its middleware

--- src/modules/users/middlewares/isAuthenticated.ts
+++ src/modules/users/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -17,7 +23,14 @@ export default function isAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    verify(token, authConfig.jwt.secret);
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch (e) {
     throw new AppError('Invalid JWT Token');
